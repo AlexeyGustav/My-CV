a15b7b65fb16a67a78c205c1750d39aaf79411bc
Alexey <gustavaleksej9@gmail.com>
2022-11-26 10:17:37 +0600

    emailphp

--- mail.php
+++ mail.php
@@ -7,7 +7,7 @@ $name = trim($_POST["name"]);
 $phone = trim($_POST["email"]);
 $message = "Имя: $name \nТелефон: $email \nСообщение: $submit";
 
-$pagetitle = "Заявка с сайта \"$siteName\"";
+$pagetitle = "Заявка с сайта \"$siteName\"https://alexeygustav.github.io/My-CV";
 mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
 
 ?>
