36e38eb3cc68c5ca1203c909d7b6d785c1df4fee
Alexey <gustavaleksej9@gmail.com>
2022-11-26 11:12:34 +0600

    error corection

--- mail.php
+++ mail.php
@@ -4,8 +4,9 @@ $recepient = "gustavaleksej9@gmail.com";
 $siteName = "CV";
 
 $name = trim($_POST["name"]);
-$phone = trim($_POST["email"]);
-$message = "Имя: $name \nТелефон: $email \nСообщение: $submit";
+$email = trim($_POST["email"]);
+$submit = trim($_POST["submit"]);
+$message = "Имя: $name \nemail: $email \nСообщение: $submit";
 
 $pagetitle = "Заявка с сайта \"$siteName\"https://alexeygustav.github.io/My-CV";
 mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
--- main.js
+++ main.js
@@ -18,14 +18,3 @@ $(document).ready(function() {
 	});
 });
 
-// // Закрыть попап «спасибо»
-// $('.js-close-thank-you').click(function() { // по клику на крестик
-// 	$('.js-overlay-thank-you').fadeOut();
-// });
-
-$(document).mouseup(function (e) { // по клику вне попапа
-    var popup = $('.popup');
-    if (e.target!=popup[0]&&popup.has(e.target).length === 0){
-        $('.js-overlay-thank-you').fadeOut();
-    }
-});
