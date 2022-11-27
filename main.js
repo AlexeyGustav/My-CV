8a118c04fad8d4dd77270ee5f171d09e436bc348
Alexey <gustavaleksej9@gmail.com>
2022-11-27 12:00:20 +0600

    new form

--- main.js
+++ main.js
@@ -1,32 +1,22 @@
 $(document).ready(function() {
-	$('#form').submit(function() { // проверка на пустоту заполненных полей. Атрибут html5 — required не подходит (не поддерживается Safari)
-		if (document.form.name.value == '' || document.form.phone.value == '' ) {
-			valid = false;
-			return valid;
-		}
+
+	//E-mail Ajax Send
+	$("form").submit(function() { //Change
+		var th = $(this);
 		$.ajax({
 			type: "POST",
-			url: "/mail.php",
-			data: $(this).serialize()
+			url: "mail.php", //Change
+			data: th.serialize()
 		}).done(function() {
-			$('.js-overlay-thank-you').fadeIn();
-			$(this).find('input').val('');
-			$('#form').trigger('reset');
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
 		});
 		return false;
 	});
-});
 
-// Закрыть попап «спасибо»
-$('.js-close-thank-you').click(function() { // по клику на крестик
-	$('.js-overlay-thank-you').fadeOut();
-});
-
-$(document).mouseup(function (e) { // по клику вне попапа
-    var popup = $('.popup');
-    if (e.target!=popup[0]&&popup.has(e.target).length === 0){
-        $('.js-overlay-thank-you').fadeOut();
-    }
 });
 
 // Маска ввода номера телефона (плагин maskedinput)
